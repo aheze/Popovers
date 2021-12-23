32303a85868f27b8668b407c63ba23b711852813
Zheng <aheze@getfind.app>
2021-12-23 12:42:57 -0800

    Document Popovers.swift

--- Sources/Popovers/PopoverReader.swift
+++ Sources/Popovers/PopoverReader.swift
@@ -8,15 +8,29 @@
 
 import SwiftUI
 
+/**
+ Read the popover's context from within its `view` or `background`.
+ 
+ Use this just like `GeometryReader`.
+ 
+ **Warning:** This **must** be placed inside a popover's `view` or `background`.
+ */
 public struct PopoverReader<Content: View>: View {
+    
+    /// Read the popover's context from within its `view` or `background`.
     public init(@ViewBuilder view: @escaping (Popover.Context) -> Content) {
         self.view = view
     }
     
+    /// The parent view.
     @ViewBuilder var view: (Popover.Context) -> Content
+    
+    /// The popover's context (passed down from `Popover.swift`).
     @EnvironmentObject var context: Popover.Context
 
     public var body: some View {
+        
+        /// Pass the context down.
         view(context)
     }
 }
--- Sources/Popovers/Popovers.swift
+++ Sources/Popovers/Popovers.swift
@@ -8,27 +8,37 @@
 
 import SwiftUI
 
+/**
+ The main access point for the Popovers library.
+ */
 public struct Popovers {
     
-    /// there might be multiple scenes on iPad, so make a window for each one
+    /**
+     The windows to hold the popovers.
+     
+     There might be multiple scenes on iPad, so make a window for each one.
+     */
     public static var windows: [UIWindowScene: PopoverContainerWindow] = [:]
     
+    /// The bounds of the current window.
     public static var windowBounds: CGRect {
-        return getWindow().bounds
+        return getWindow().0.bounds
     }
     
-    /// window bounds with safe area
+    /// The frame of the current window with the safe area applied.
     public static var safeWindowFrame: CGRect {
-        let window = getWindow()
+        let window = getWindow().0
         let safeAreaFrame = window.safeAreaLayoutGuide.layoutFrame
         return safeAreaFrame
     }
     
+    /// The view model for the popovers.
     public static var model: PopoverModel = {
         let model = PopoverModel()
         return model
     }()
     
+    /// The popovers that are currently presented.
     public static var current: [Popover] {
         get {
             model.popovers
@@ -37,6 +47,11 @@ public struct Popovers {
         }
     }
     
+    /**
+     Enable or disable popover dragging globally.
+     
+     This is useful if you have nested sliders or other gestures that interfere with the popover's rubber banding. Set this to `true` to enable dragging, `false` to disable dragging.
+     */
     public static var draggingEnabled: Bool {
         get {
             Popovers.model.popoversDraggable
@@ -45,44 +60,87 @@ public struct Popovers {
         }
     }
     
-    public static func getWindow() -> PopoverContainerWindow {
+    /**
+     Get the current window.
+     - Returns: A tuple `(window, reused)`.
+     
+     - `window` - the current window.
+     - `reused` - indicates if an existing window was reused.
+     */
+    public static func getWindow() -> (window: PopoverContainerWindow, reused: Bool) {
         if let currentScene = UIApplication.shared.currentWindowScene {
+            
+            /// Get the window for the current scene.
             if let window = windows[currentScene] {
-                return window
+                return (window, true)
             } else {
+                
+                /// No popover window exists yet, make one.
                 let window = PopoverContainerWindow(
                     popoverModel: model,
                     scene: currentScene
                 )
+                
                 windows[currentScene] = window
-                return window
+                return (window, false)
             }
         } else {
+            
+            /// No popover window exists yet and there is no scene active, make one.
+            /// This is highly unlikely to be called.
             let window = PopoverContainerWindow(
                 popoverModel: model,
                 scene: nil
             )
-            return window
+            
+            return (window, false)
         }
     }
     
+    /**
+     Present a popover.
+     */
     public static func present(_ popover: Popover) {
 
+        /// Make sure the view model exists.
         _ = model
         
-        _ = getWindow()
+        /// Make sure a window exists.
+        let (_, reused) = getWindow()
         
-        /// make sure the window is set up the first time
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
-            
+        /// Configure and present the popover.
+        func presentPopover() {
+            /// Create a transaction for the presentation animation.
             let transaction = Transaction(animation: popover.attributes.presentation.animation)
+            
+            /// Inject the transaction into the popover, so following frame calculations are animated smoothly.
             popover.context.transaction = transaction
+            
             withTransaction(transaction) {
+                
+                /// Add the popover to the container view.
                 current.append(popover)
+            }
+        }
+        
+        /// Directly present the popover if an existing window was reused.
+        if reused {
+            presentPopover()
+        } else {
+            
+            /// Otherwise, make sure the window is set up the first time and ready for SwiftUI.
+            /// Without a delay, SwiftUI won't apply the animation.
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.01) {
+                presentPopover()
             }
         }
     }
     
+    /**
+     Dismiss a popover.
+     
+     Provide `transaction` to override the default dismissal transition.
+     */
     public static func dismiss(_ popover: Popover, transaction: Transaction? = nil) {
         if let popoverIndex = index(of: popover) {
             popover.context.dismissed?()
@@ -95,63 +153,95 @@ public struct Popovers {
         }
     }
     
+    /// Dismiss all popovers.
     public static func dismissAll() {
         for popover in current.reversed() {
             dismiss(popover)
         }
     }
     
+    /**
+     Replace a popover with another popover smoothly.
+     
+     This is what `.popover(selection:tag:attributes:view)` in SwiftUI uses.
+     */
     public static func replace(_ oldPopover: Popover, with newPopover: Popover) {
         _ = getWindow()
         
+        /// Get the index of the previous popover.
         if let oldPopoverIndex = index(of: oldPopover) {
 
-            let currentContext = current[oldPopoverIndex].context
+            /// Get the old popover's context.
+            let oldContext = current[oldPopoverIndex].context
             
+            /// Create a new transaction for the replacing animation.
             let transaction = Transaction(animation: newPopover.attributes.presentation.animation)
             
-            /// set this for future animations
+            /// Inject the transaction into the new popover, so following frame calculations are animated smoothly.
             newPopover.context.transaction = transaction
             
-            /// use same ID so that SwiftUI animates the change
-            newPopover.context.id = currentContext.id
+            /// Use same ID so that SwiftUI animates the change.
+            newPopover.context.id = oldContext.id
             
             withTransaction(transaction) {
-                newPopover.setSize(currentContext.size)
+                
+                /// Temporarily use the same size for a smooth animation.
+                newPopover.setSize(oldContext.size)
+                
+                /// Replace the old popover with the new popover.
                 current[oldPopoverIndex] = newPopover
             }
         }
     }
     
-    /// optional refresh
-    public static func refresh(with transaction: Transaction? = nil) {
+    /**
+     Refresh the popovers with a new transaction.
+     
+     This is called when a popover's frame is being calculated.
+     */
+    public static func refresh(with transaction: Transaction?) {
+        
+        /// Set each popovers's transaction to the new transaction to keep the smooth animation.
         for popover in current {
             popover.context.transaction = transaction
         }
+        
+        /// Update all popovers.
         model.refresh()
     }
+    
+    /**
+     Update all popover frames.
+     
+     This is called when the device rotates or has a bounds change.
+     */
     public static func updateFrames() {
         for popover in current {
             if 
                 case .relative(let popoverAnchors) = popover.attributes.position,
                 popoverAnchors == [.center]
             {
-                /// for some reason, relative positioning + .center does not need to be on the main queue
+                /// For some reason, relative positioning + `.center` doesn't need to be on the main queue to have a size change.
                 popover.setSize(popover.context.size)
             } else {
                 
-                /// must be on main queue to get a different SwiftUI render loop
+                /// Must be on the main queue to get a different SwiftUI render loop
                 DispatchQueue.main.async {
                     popover.setSize(popover.context.size)
                 }
             }
         }
+        
+        /// Update all popovers.
         model.refresh()
     }
     
+    /// Get a currently-presented popover with a tag. Returns `nil` if no popover with the tag was found.
     public static func popover(tagged tag: String) -> Popover? {
         return current.first(where: { $0.attributes.tag == tag })
     }
+    
+    /// Get the index in the `current` array for a popover. Returns `nil` if the popover is not in the `current` array.
     public static func index(of popover: Popover) -> Int? {
         return current.indices.first(where: { current[$0] == popover })
     }
