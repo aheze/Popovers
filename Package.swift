20936ae55297fcf8d799a2b3c937af6b41cdda3f
Zheng <aheze@getfind.app>
2021-12-23 13:57:24 -0800

    Lower deployment version to iOS 13

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Popovers",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
