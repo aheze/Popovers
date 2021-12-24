05e261a18058b2bf609578984aeef09c232373c4
Zheng <aheze@getfind.app>
2021-12-23 18:10:30 -0800

    Reorganize folders

--- Package.swift
+++ Package.swift
@@ -24,7 +24,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Popovers",
-            dependencies: []
+            dependencies: [],
+            path: "Source"
         )
     ]
 )
