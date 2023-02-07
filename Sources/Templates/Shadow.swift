fb9f058a553fa108f429e3caf657f9bf5ca4218c
Andrew Zheng <aheze@getfind.app>
2023-02-07 00:07:09 -0800

    Make shadow color always black by default

--- Sources/Templates/Shadow.swift
+++ Sources/Templates/Shadow.swift
@@ -13,7 +13,7 @@ public extension Templates {
     /// A convenient way to apply shadows. Access using the `.popoverShadow()` modifier.
     struct Shadow {
         /// The shadow color.
-        public var color = Color(.label.withAlphaComponent(0.3))
+        public var color = Color.black.opacity(0.25)
     
         /// The shadow radius.
         public var radius = CGFloat(0)
@@ -25,14 +25,14 @@ public extension Templates {
         public var y = CGFloat(0)
     
         public static var system = Self(
-            color: Color(.label.withAlphaComponent(0.25)),
+            color: Color.black.opacity(0.25),
             radius: 40,
             x: 0,
             y: 4
         )
 
         public init(
-            color: Color = Color(.label.withAlphaComponent(0.3)),
+            color: Color = Color.black.opacity(0.25),
             radius: CGFloat = CGFloat(0),
             x: CGFloat = CGFloat(0),
             y: CGFloat = CGFloat(0)
